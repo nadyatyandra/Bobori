eb92c4ff9535baf5f7c6b91a4264ab591d560457
leo <leoharnadi@gmail.com>
2023-04-18 08:51:15 +0700

    Initial Commit

---
+++ Mini1/Mini1App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini1App.swift
+//  Mini1
+//
+//  Created by Leo Harnadi on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
