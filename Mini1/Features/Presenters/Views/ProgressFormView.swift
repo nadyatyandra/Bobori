ccafd89da571cc95e65432099f119b0153235149
Nadya Tyandra <nadyatyandra05@gmail.com>
2023-05-03 10:48:52 +0700

    wheel picker

--- Mini1/Features/Presenters/Views/ProgressFormView.swift
+++ Mini1/Features/Presenters/Views/ProgressFormView.swift
@@ -12,8 +12,8 @@ struct ProgressFormView: View {
     @State private var bedTime: Date = Date()
     @State private var date: Date = Date()
     @State private var distance = ""
-    @State private var isShowingAlert = false
     @Environment(\.presentationMode) var presentationMode
+    var distances = ["Besides crib", "Halfway towards door", "Next to doorway", "Outside the room with opened door", "Outside the room with closed door"]
     
     var body: some View {
         NavigationView {
@@ -32,27 +32,26 @@ struct ProgressFormView: View {
                             .datePickerStyle(WheelDatePickerStyle())
                     }
                     
-                    TextField("Entry distance", text: $distance).padding().background(Color(red: 239/255, green: 243/255, blue: 244/255))
+                    Text("Entry distance")
+                    Picker("Please choose a distance", selection: $distance) {
+                        ForEach(distances, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.wheel)
                     
                     Button(action: {
-//                        if !self.date.isEmpty && !self.bedHour.isEmpty {
-//                        self.entryController.createEntry(title: self.title, entryDescription: self.description)
                         self.entryViewModel.createEntry(date: self.date, bedTime: self.bedTime, distance: self.distance)
-//                            self.title = ""
-//                            self.description = ""
-//
-//                            self.presentationMode.wrappedValue.dismiss()
-//                        } else {
-//                            self.isShowingAlert = true
-//                        }
-                          
                     }) {
                         Text("Save")
-                        }.padding().background(Color.green).clipShape(RoundedRectangle(cornerRadius: 10)).foregroundColor(.white).shadow(color: Color.gray, radius: 5)
-                    Spacer()
-                    }.padding().navigationBarTitle("Add Entry", displayMode: .inline).alert(isPresented: $isShowingAlert) {
-                Alert(title: Text("Warning"), message: Text("Please make sure both the title and the description aren't empty!"), dismissButton: .default(Text("OK!")))
-                }
+                        }
+                        .padding()
+                        .background(Color.green)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .foregroundColor(.white)
+                        .shadow(color: Color.gray, radius: 5)
+                    }
+                .padding()
             }
         }
     }
