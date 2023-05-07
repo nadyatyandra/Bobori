5de1f789854977f8cffccbaa4cbc72a000982638
jessicarachel <jessicarachel03@gmail.com>
2023-05-07 09:00:03 +0700

    song added

--- Mini1/Features/Presenters/Components/MusicPlayerModel.swift
+++ Mini1/Features/Presenters/Components/MusicPlayerModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct MusicPlayerModel {
-    let musicList: [String] = ["Song 1", "Song 2", "Song 3", "Song 4"]
+    let musicList: [String] = ["Song 1", "Song 2", "Song 3", "Song 4", "Song 5"]
     var lastDate: Date? = nil
     var selectedSong: String = "Song 1"
 }
