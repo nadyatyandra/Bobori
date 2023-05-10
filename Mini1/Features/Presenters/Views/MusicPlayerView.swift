073fa5721f6ad1c671a6cf49833fcb8c1b70929b
leo <leoharnadi@gmail.com>
2023-05-10 23:04:04 +0700

    Removed unnecessary comments

--- Mini1/Features/Presenters/Views/MusicPlayerView.swift
+++ Mini1/Features/Presenters/Views/MusicPlayerView.swift
@@ -8,11 +8,9 @@ import SwiftUI
 
 struct PausableRotation: GeometryEffect {
   
-    // this binding is used to inform the view about the current, system-computed angle value
     @Binding var currentAngle: CGFloat
     private var currentAngleValue: CGFloat = 0.0
 
-    // this tells the system what property should it interpolate and update with the intermediate values it computed
     var animatableData: CGFloat {
         get { currentAngleValue }
         set { currentAngleValue = newValue }
@@ -23,22 +21,12 @@ struct PausableRotation: GeometryEffect {
         self._currentAngle = currentAngle
     }
 
-    // this is the transform that defines the rotation
     func effectValue(size: CGSize) -> ProjectionTransform {
 
-    // this is the heart of the solution:
-    //   reporting the current (system-computed) angle value back to the view
-    //
-    // thanks to that the view knows the pause position of the animation
-    // and where to start when the animation resumes
-    //
-    // notice that reporting MUST be done in the dispatch main async block to avoid modifying state during view update
-    // (because currentAngle is a view state and each change on it will cause the update pass in the SwiftUI)
     DispatchQueue.main.async {
         self.currentAngle = currentAngleValue
     }
 
-    // here I compute the transform itself
         let xOffset = size.width / 2
         let yOffset = size.height / 2
         let transform = CGAffineTransform(translationX: xOffset, y: yOffset)
